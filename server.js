58150a52ea000378f47f6684ab3464b87e66de83
bdlewis30 <32585973+bdlewis30@users.noreply.github.com>
2018-02-15 10:01:01 -0700

    first commit

--- server.js
+++ server.js
@@ -1,17 +1,32 @@
 
 const express = require('express')
-, app = express()
-, axios = require('axios')
-, port = 8081;
+    , app = express()
+    , axios = require('axios')
+    , port = 8081;
 
 // Hit the following api to get Star Wars characters and return an array with 40 characters. Each page will only return 10 so you will need to find a way to get 40. 
 
 // https://swapi.co/api/people/?page=1
 
+// Recursion example.
 app.get('/characters', (req, res) => {
+    var page = 1;
+    var arr = [];
+    (function getCharacters() {
+        if(arr.length < 40){
+            axios.get(`https://swapi.co/api/people/?page=${page}`).then(res => {
+                arr = [...arr, ...res.data.results] //... spread operactor
+                page++
+                getCharacters()
+            })
+        }
+        else {
+            console.log(arr.length)
+            res.send(arr)
+        }
 
+    })() // IIFE
 
-    
 })
 
 
@@ -19,4 +34,4 @@ app.get('/characters', (req, res) => {
 
 app.listen(port, () => {
     console.log(`Listening on port ${port}`);
-    })
+})
